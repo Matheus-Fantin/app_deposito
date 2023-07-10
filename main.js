ea114e8be9e888cb26907e30cd4f50123841f07c
Matheus-Fantin <130580170+Matheus-Fantin@users.noreply.github.com>
2023-07-10 11:28:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form-deposito');
 
 function validaNome(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split(' ');
-    return nomeComoArray.lenght >= 2;
+    return nomeComoArray.length >= 2;
 }
 
 form.addEventListener('submit', function(e) {
